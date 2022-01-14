bf38ef15f0ebd92ccaf1154b2036101d03f46a42
Diohandres <diohandres1703@hotmail.com>
2022-01-14 16:48:56 -0400

    EnableAccount Metamask

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+
+  let enableEthereumButton = useRef(null);
+  const [showAccount, setShowAccount] = useState('Undefined');
+
+  const getAccount = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    setShowAccount(account);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Antonio es una perra
+          RealStake
         </p>
-        <a
-          className="App-link"
-          href="https://rubias19.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Da click aqui para que lo mames
-        </a>
+        {showAccount.length < 10 ? (
+          <button onClick={getAccount} ref={enableEthereumButton} className='btn btn-success'>Enable Ethereum</button>
+        ) : null}
+        <h2>Account: <span>{ showAccount }</span></h2>        <i className="fas fa-solar-panel    "></i>
       </header>
     </div>
   );
